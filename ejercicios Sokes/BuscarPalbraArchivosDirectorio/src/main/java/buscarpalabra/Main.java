48c818557f50eaaf355a726f581c2a26a3b1f3e5
Andrey3681 <117780962+Andrey3681@users.noreply.github.com>
2023-11-12 16:15:19 -0500

    load

--- ejercicios Sokes/BuscarPalbraArchivosDirectorio/src/main/java/buscarpalabra/Main.java
+++ ejercicios Sokes/BuscarPalbraArchivosDirectorio/src/main/java/buscarpalabra/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[]arg){
         HiloBuscar hiloBuscar=new HiloBuscar();
-        hiloBuscar.start("juan","BuscarPalbraArchivosDirectorio/src/main/resources/Archivos");
+
+        //poner la ruta del directorio que desea revisar los archivos txt ( no tiene porque ser del repositorio siempre y cuando la ruta este bien)
+        hiloBuscar.start("juan","ejercicios Sokes/BuscarPalbraArchivosDirectorio/src/main/resources/Archivos");
     }
 }
